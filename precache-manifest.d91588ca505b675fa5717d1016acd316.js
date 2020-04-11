a6011ada2da071a21ee735f8e5089a215a23f0a3
simonhaisz@gmail.com <simonhaisz@gmail.com>
2020-04-11 15:02:12 -0400

    Updates

---
+++ precache-manifest.d91588ca505b675fa5717d1016acd316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcca069b7c1db4f22e5f7eda84344466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd5440f262c6a0205909",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "76d9a86cc75d1222569d",
+    "url": "/static/js/2.00f3bd73.chunk.js"
+  },
+  {
+    "revision": "9b2f0f2c02ec15c1a684dfd9b7e38072",
+    "url": "/static/js/2.00f3bd73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd5440f262c6a0205909",
+    "url": "/static/js/main.7a69d90f.chunk.js"
+  },
+  {
+    "revision": "f412a20902113d76ded2",
+    "url": "/static/js/runtime-main.031057c5.js"
+  }
+]);
