56b4560360f08ba7216405b22e99e0d5b0139038
Simon Haisz <simonhaisz@gmail.com>
2020-11-14 16:07:56 -0500

    Import-export functionality (json)

---
+++ src/component/ExportButton.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import PublishIcon from "@material-ui/icons/Publish";
+import Tooltip from "@material-ui/core/Tooltip";
+
+type Props = {
+	filename: string;
+	generateData: () => string;
+};
+const ExportButton: FC<Props> = (props: Props) => {
+	const { filename, generateData } = props;
+
+	const handleClick = () => {
+		const data = generateData();
+		const element = document.createElement('a');
+		element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(data));
+		element.setAttribute('download', filename);
+	
+		element.style.display = 'none';
+		document.body.appendChild(element);
+	
+		element.click();
+	
+		document.body.removeChild(element);
+	};
+	return (
+		<Tooltip title="Export Character">
+			<IconButton
+				onClick={handleClick}
+				color="secondary"
+			>
+				<PublishIcon />
+			</IconButton>
+		</Tooltip>
+	);
+};
+
+export default ExportButton;
